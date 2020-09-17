f1a4c85afd5780df19ded3b3bd9da073c0db40d1
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-09-17 22:12:10 +0900

    MainLibAcitivityを作成

---
+++ AndroidLibraryDataBindingKtx/mylibrary/src/main/java/net/kwmt27/mylibrary/MainLibActivity.kt
@@ -0,0 +1,11 @@
+package net.kwmt27.mylibrary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainLibActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_lib)
+    }
+}
