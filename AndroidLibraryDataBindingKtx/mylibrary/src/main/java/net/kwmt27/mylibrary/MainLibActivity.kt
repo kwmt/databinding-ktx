9d15c7821675c6c3a9c8ed9e15639cd514e04b4d
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-09-21 14:25:22 +0900

    databinding-ktxを使用

--- AndroidLibraryDataBindingKtx/mylibrary/src/main/java/net/kwmt27/mylibrary/MainLibActivity.kt
+++ AndroidLibraryDataBindingKtx/mylibrary/src/main/java/net/kwmt27/mylibrary/MainLibActivity.kt
@@ -2,10 +2,16 @@ package net.kwmt27.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wada811.databinding.dataBinding
+import net.kwmt27.mylibrary.databinding.ActivityMainLibBinding
 
 class MainLibActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainLibBinding by dataBinding()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_lib)
+
+        binding.textview.text = "Sample"
     }
 }
